3c12c60faaa1cceea98ee0715a1dcd1bfb874463
Darien Miranda <panzerfausten@gmail.com>
2015-02-16 16:52:29 -0800

    init

---
+++ generate.py
@@ -0,0 +1,31 @@
+__DATA = []
+
+with open("data.txt","r") as _DATA:
+    for _line in _DATA:
+        __DATA.append( _line.replace("\n","").strip("\t"))
+    
+    
+for _dataLine in __DATA:
+    _dataLine =  _dataLine.split("\t")
+    with  open("patron.html","r") as _FILE:
+        _FILECONTENT = []
+        _NEWFILECONTENT = []
+
+        for _line in _FILE:
+            if("-" in _line[0] ):
+                _newline = "     <div id='div_title'> Prueba %s/20</div>" % (_dataLine[0])
+                print _newline
+                _NEWFILECONTENT.append(_newline)
+            elif("**" in _line[0:2] ):
+                _newline = "                      window.location = '%s.html';" %( str ( int ( _dataLine[0])+1) )
+                _NEWFILECONTENT.append(_newline)
+            elif(_line[0] == "*"):
+                _newline = "               <button id='play_button' onclick='play_sounds(keyNoteToFreq(%s),keyNoteToFreq(%s),keyNoteToFreq(%s))'>Reproducir </button>" % (_dataLine[1],_dataLine[2],_dataLine[3])
+                _NEWFILECONTENT.append(_newline)
+            else:
+                _NEWFILECONTENT.append(_line)
+
+
+        _FILENAME = "%s.html" %( str( _dataLine[0]))
+        with open( _FILENAME,"w") as _OUTPUT_FILE:
+                _OUTPUT_FILE.writelines(_NEWFILECONTENT)
